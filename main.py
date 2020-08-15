61d8f527ab88c5d748ceee76fc65a6d2c66a4430
Xander Forrest <xander@xanderforrest.com>
2020-08-15 21:40:02 +0100

    Add skeleton code with requirements

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from secrets import token
+
+
+class STEPBot(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = STEPBot()
+client.run(token)
